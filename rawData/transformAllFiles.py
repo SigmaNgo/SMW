a35798d4eb61e876974bdf59849a317a52bd9769
JoelSalzesson <joelsalzesson@gmail.com>
2018-06-20 15:10:18 +0200

    make transform all script accept output path from command line

--- rawData/transformAllFiles.py
+++ rawData/transformAllFiles.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 print("current working directory " + os.getcwd())
 for file in os.listdir(os.getcwd()):
     if file.endswith(".doc") or file.endswith(".docx") or file.endswith(".odt"):
         print("transformming " + file)
-        os.system("python transformToCsv.py '" + file + "' transformed/")
+        os.system("python transformToCsv.py '" + file + "' " + sys.argv[1])
     else:
         print("! wrong extension, skipping " + file)
