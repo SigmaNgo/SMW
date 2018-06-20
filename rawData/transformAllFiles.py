a1d505cd138cf695088c1290177e4c68337d8495
JoelSalzesson <joelsalzesson@gmail.com>
2018-06-20 18:59:53 +0200

    update scripts; one raw data file was not valid - fixed; added transformed csv files

--- rawData/transformAllFiles.py
+++ rawData/transformAllFiles.py
@@ -6,6 +6,7 @@ import sys
 
 print("current working directory " + os.getcwd())
 for file in os.listdir(os.getcwd()):
+    print("--------------------------------------")
     if file.endswith(".doc") or file.endswith(".docx") or file.endswith(".odt"):
         print("transformming " + file)
         os.system("python transformToCsv.py '" + file + "' " + sys.argv[1])
--- rawData/transformToCsv.py
+++ rawData/transformToCsv.py
@@ -8,7 +8,7 @@ import re
 import csv #for more go here https://pymotw.com/2/csv/
 import inspect
 
-pattern = re.compile('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})[\r\n]+(INT-\d{4,7}\/\d{2})[\r\n]+(.*)[\r\n]+(.*)[\r\n]+(.*)[\r\n]+(.*)[\r\n]+(\d*)[\r\n]+(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})')
+pattern = re.compile('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})[\s]+(INT-\d{4,7}\/\d{2})[\s]+(.*)[\s]+(.*)[\s]+(.*)[\s]+(.*)[\s]+(\d*)[\s]+(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})')
 
 inputFilePatch = sys.argv[1]
 outputFilePatchRelativeToinputFilePatch = sys.argv[2]
@@ -18,19 +18,20 @@ inputFileSplited = os.path.splitext(basename)
 outputFileName = inputFileSplited[0] + '.csv'
 outputFilePath = outputFilePatchRelativeToinputFilePatch + outputFileName
 
-print("inputFilePatch      " + inputFilePatch)
-print("outputFilePatch     " + outputFilePatchRelativeToinputFilePatch)
-print("basename            " + basename)
-print("dirname             " + dirname)
-print("inputFileSplited[0] " + inputFileSplited[0])
-print("inputFileSplited[1] " + inputFileSplited[1])
-print("outputFileName      " + outputFileName)
-print("outputFilePath      " + outputFilePath)
+#print("inputFilePatch      " + inputFilePatch)
+#print("outputFilePatch     " + outputFilePatchRelativeToinputFilePatch)
+#print("basename            " + basename)
+#print("dirname             " + dirname)
+#print("inputFileSplited[0] " + inputFileSplited[0])
+#print("inputFileSplited[1] " + inputFileSplited[1])
+#print("outputFileName      " + outputFileName)
+#print("outputFilePath      " + outputFilePath)
 
 f = open(outputFilePath, 'wt')
 try:
     writer = csv.writer(f)
-    textToBeExamined = textract.process(inputFilePatch, 'utf8')
+    textToBeExamined = textract.process(inputFilePatch, 'utf8').replace("|", "\n") #the replace is needed because (for unknown reason) .doc files has this characted while .docx does not
+    print(len(re.findall(pattern, textToBeExamined)))
     for result in re.findall(pattern, textToBeExamined):
         writer.writerow(result)
 
