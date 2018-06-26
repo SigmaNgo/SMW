2b04617a6769c60a80a8f008c29dce92e744f50c
JoelSalzesson <joelsalzesson@gmail.com>
2018-06-26 15:23:17 +0200

    ignore doc; add 3 of transformed doc->docx

--- rawData/transformAllFiles.py
+++ rawData/transformAllFiles.py
@@ -7,7 +7,7 @@ import sys
 print("current working directory " + os.getcwd())
 for file in os.listdir(os.getcwd()):
     print("--------------------------------------")
-    if file.endswith(".doc") or file.endswith(".docx") or file.endswith(".odt"):
+    if file.endswith(".docx") or file.endswith(".odt"):
         print("transformming " + file)
         os.system("python transformToCsv.py '" + file + "' " + sys.argv[1])
     else:
