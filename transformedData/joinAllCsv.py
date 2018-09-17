aea99962edddd44522f7f53c6a4fc99acfaf7de9
JoelSalzesson <joelsalzesson@gmail.com>
2018-09-17 07:35:53 +0200

    add 2 new csv; change screipts; add new all-joined csv

--- transformedData/joinAllCsv.py
+++ transformedData/joinAllCsv.py
@@ -5,10 +5,15 @@ import os
 import sys
 
 print("current working directory " + os.getcwd())
+
+file = open(sys.argv[1], 'w')
+file.write("startDate,id,description,type,address,result,fine,endDate\n")
+file.close()
+
 for file in os.listdir(os.getcwd()):
     print("--------------------------------------")
-    if file.endswith(".csv"):
+    if file.endswith(".csv") and file != sys.argv[1]:
         print("joining " + file)
         os.system("cat '" + file + "' >> " + sys.argv[1])
     else:
-        print("! wrong extension, skipping " + file)
+        print("! skipping " + file)
